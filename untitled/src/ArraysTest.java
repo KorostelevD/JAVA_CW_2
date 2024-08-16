11be4e479c7ea19f79fa88eb2fbd00d274f18dd6
Dmytro Korostelyov <m0501543116@gmail.com>
2024-08-16 21:07:05 +0300

    Init5

--- untitled/src/ArraysTest.java
+++ untitled/src/ArraysTest.java
@@ -15,6 +15,19 @@ public class ArraysTest {
         int[] j = {10,20,30};
         System.out.println(Arrays.toString(j));
 
+        //
+        int[] ar1 = {11,22,33};
+        int[] ar2 = new int[ar1.length];
+        //
+        System.arraycopy(ar1,0,ar2,0, ar1.length);
+        System.out.println(Arrays.toString(ar2));
 
+        //2
+        int[] ar3 = Arrays.copyOf(ar1, ar1.length);
+        int[] ar4 = Arrays.copyOfRange(ar1, 0, 1);
+        //
+        int[] ar5 = ar1.clone();
+        System.out.println(Arrays.toString(ar5));
+        //   
     }
 }
